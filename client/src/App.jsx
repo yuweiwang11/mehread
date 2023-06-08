434fa239094e7d0cd16408386e3d006f5c6eb6d4
Yuwei Wang <w67238@outlook.com>
2023-06-08 16:13:25 +1200

    implement routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <>
-      <div className="bg-red-500">hello</div>
+      <Routes>
+        <Route>
+          {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/account/bookings/:id" element={<BookingPage />} /> */}
+        </Route>
+      </Routes>
     </>
   )
 }
