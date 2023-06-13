9bc2cd85b7f5ee4c83287870826ced29168b3ad4
Yuwei Wang <w67238@outlook.com>
2023-06-13 12:58:53 +1200

    add login page

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -2,10 +2,17 @@ import { Link } from 'react-router-dom'
 
 export default function Nav() {
   return (
-    <div className="cursor-pointer w-32">
-      <Link to={'/'}>
-        <img className="w-32 mb-8 mt-8" src="../logo/MEH_v5.png" alt="logo" />
-      </Link>
-    </div>
+    <>
+      <div className="flex">
+        <div className="cursor-pointer w-32">
+          <Link to={'/'}>
+            <img className="w-32 mb-8 mt-8" src="../logo/MEH_v5.png" alt="logo" />
+          </Link>
+        </div>
+        <div className="mt-2 ml-auto p-5 underline">
+          <Link to={'/mehread/login'}>login</Link>
+        </div>
+      </div>
+    </>
   )
 }
