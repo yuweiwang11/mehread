f8d5f1830eb16dfe915025346e2f2f2bfa5d0e79
Yuwei Wang <w67238@outlook.com>
2023-06-14 16:04:30 +1200

    add icon to login link

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -9,8 +9,15 @@ export default function Nav() {
             <img className="w-32 mb-8 mt-8" src="../logo/MEH_v5.png" alt="logo" />
           </Link>
         </div>
-        <div className="mt-2 ml-auto p-5 underline">
-          <Link to={'/mehread/signin'}>login</Link>
+        <div className=" mt-2 ml-auto p-5 underline">
+          <Link to={'/mehread/signin'}>
+            <img
+              className="inline-flex w-6 mr-1 -mt-2"
+              src="../logo/usericon_lined.png"
+              alt="user_icon"
+            />
+            Log In
+          </Link>
         </div>
       </div>
     </>
